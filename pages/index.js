7026904232713cbe5ae79740e85ef5055b4a5eda
Ashwin D Kini <ashwindkini@users.noreply.github.com>
2021-01-06 14:00:49 +0530

    changed copy in homepage

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The best space jellyfish swag in the universe.
         </p>
 
         <ul className={styles.grid}>
